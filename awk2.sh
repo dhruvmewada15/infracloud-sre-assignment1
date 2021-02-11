7e056e88b638345c7bd943cafcda3fff6ca5363c
dhruvmewada15 <dhruvmewada@outlook.com>
2021-02-11 23:35:33 +0530

    updated sum logic

--- awk2.sh
+++ awk2.sh
@@ -1,6 +1,2 @@
-awk '{ sum = $2 + $3 + $4; sum = sum / 3;
-if(sum > 50) print $1 " : Pass " sum;
-else print $1 " : Fail " sum}'
-
 awk '{ if ($1>=50 && $2>=50 && $3>=50) print $1 " : Pass ";
 else print $1 " : Fail "}'
